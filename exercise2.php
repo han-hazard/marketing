9a85f8ff429555aaca8a1b88de994ef76175c0d4
han-it-vn <trandinhhan30081996@gmail.com>
2021-06-15 15:44:51 +0700

    update

--- exercise2.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    require "functions.php";
-
-    echo "tổng của mảng là:" . sum() . "<br>";
-    echo "số lớn nhất của mảng là: " . maxarr(). "<br>";
-    echo "số nhỏ nhất của mảng là: " . minarr(). "<br>";
-?>
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-   
-   $sum = array(1,3,5,7,9);
-   $tong=0;
-   for($i=0;$i<count($array);$i++){
-       $tong=$tong+$sum[$i];
-   }
-   echo "tổng của mảng là : $tong";
-?>
